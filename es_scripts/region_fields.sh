b3a52567c738807ac8839594190f25791ee34048
blueOwl <me@blueowl.cn>
2019-11-19 12:45:24 -0800

    add source field filter example

---
+++ es_scripts/region_fields.sh
@@ -0,0 +1,10 @@
+curl -XPOST 'localhost:9200/vs-index/_search?pretty' \
+-H 'Content-Type: application/json' \
+-d '{
+"_source": ["ANNOVAR_ensembl_GO_biological_process_complete_list_id","pos"],
+"query": { 
+    "bool": {
+	"filter": [
+		{"term": {"chr":"2"}},
+		{"range" : { "pos" : { "gte" : 10, "lte" : 20000 } }}]
+}}}'
