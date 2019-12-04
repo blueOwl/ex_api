419a815c70fdf7ccb5858ef68a5e914a733f91ca
blueOwl <me@blueowl.cn>
2019-12-04 11:20:07 -0800

    change tree node id type

--- tree.py
+++ tree.py
@@ -1,14 +1,16 @@
 class Annotation_tree_node:
-	def __init__(self, nid=0, parent_id=0, info='', name="root"):
+	def __init__(self, nid=0, parent_id=0, info='', name="root", leaf=False):
 		self.id = nid
 		self.parent_id = parent_id
 		self.info = info
 		self.name = name
+		self.leaf = leaf
 	def get_dic(self):
 		return {'id' : self.id,
 			'name' : self.name,
 			'detail' : self.info,
-			'parent_id' : self.parent_id} 
+			'parent_id' : self.parent_id,
+                        'leaf':self.leaf} 
 
 def dict_to_tree(dic):
     uid = 1
@@ -19,7 +21,7 @@ def dict_to_tree(dic):
         tree_dic[k] = node
         uid += 1
         for anno_name in dic[k]:
-            node = Annotation_tree_node(nid=uid, name=anno_name, info=anno_name, parent_id=tree_dic[k].id)
+            node = Annotation_tree_node(nid=uid, name=anno_name, info=anno_name, parent_id=tree_dic[k].id, leaf=True)
             tree_dic[anno_name] = node
             uid += 1
     print(set([tree_dic[k].parent_id for k in tree_dic]))
